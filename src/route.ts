f88feaa465fefaa1030cb01ea1bed239dfcae411
romelt777 <romel.tan777@gmail.com>
2026-04-01 14:25:57 -0300

    now using firebase in route, not tested yet

--- src/route.ts
+++ src/route.ts
@@ -2,21 +2,13 @@
 import express from "express";
 import type { Request, Response } from "express";
 import multer from "multer";
-
-//file will be uploaded from flutter
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads"); //destination where to store image
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + ".jpg"); //timestampvalue.jpg, this will be file name 
-    }
-})
+import { getBucket } from "./firebase.js";
 
 
-//storage must be provided to multer
+
+//memory storage will keeofile in memory as buffer
 const upload = multer({
-    storage: storage
+    storage: multer.memoryStorage()
 })
 
 
@@ -24,10 +16,31 @@ const router = express.Router();
 
 //post request, with API name, thsis will be same endpoint to call in flutter
 //img is a key
-router.route("/addImages").post(upload.single("img"), (req: Request, res: Response) => {
+router.route("/addImages").post(upload.single("img"), async (req: Request, res: Response) => {
     //return response to flutter app
     try {
-        return res.json({ path: req.file?.filename }); // returning path of image to flutter
+        if (!req.file) {
+            return res.status(400).json({ error: "No file received" });
+        }
+
+        //firebase
+        const bucket = getBucket();
+        const fileName = `images/${Date.now()}.jpg`
+        const file = bucket.file(fileName);
+
+        //save the file buffer directly to firebase storage
+        await file.save(req.file.buffer, {
+            metadata: { contentType: "image/jpg" },
+        });
+
+        //make file publically accessible
+        await file.makePublic();
+
+        //constructing the public url
+        const publicUrl = `https://storage.googleapis.com/${process.env.FIREBASE_STORAGE_BUCKET}/${fileName}`;
+
+
+        return res.json({ path: publicUrl }); // returning path of image to flutter
     }
     catch (e) {
         //e is unknown and can be many types, check if error object before using
