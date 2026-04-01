eef7a573c1b4054aec3ecc6dacbdce9fb48f7c13
romelt777 <romel.tan777@gmail.com>
2026-04-01 14:09:53 -0300

    writing firebase file, dotenv package added

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+import admin from "firebase-admin"; //server side firebase library
+import { readFileSync } from "fs"; //for reading local firebase credentials
+import "dotenv/config";
+
+
+//reading credential
+const serviceAccount = JSON.parse(
+    readFileSync("./serviceAccountKey.json", "utf-8")
+);
+
+let bucketName: string = "";
+if (process.env.FIREBASE_STORAGE_BUCKET) {
+    bucketName = process.env.FIREBASE_STORAGE_BUCKET;
+}
+
+//intializing with account credentiall, and bucket name
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: bucketName,
+})
+
+export const bucket = admin.storage().bucket();
